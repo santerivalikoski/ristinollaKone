bd9221683b4baa83237234befcf395c33608c972
santerivalikoski <santeri.valikoski@gmail.com>
2020-04-13 19:43:45 +0300

    git

--- src/App.js
+++ src/App.js
@@ -14,34 +14,57 @@ const App = () => {
   }
 
   const handleClick = (i) => {
-    console.log('HANDLEKLIK ALKU', i, 'board: ', boardSize[i])
+    // console.log('HANDLEKLIK ALKU', i, 'board: ', boardSize[i])
+    if(i<0) alert('ERROR HANDLECLICKISSÄ')
     if (boardSize[i] === '' && !winner) {
-      console.log('handleClick', i)
+      // console.log('handleClick', i)
       let changedBoard = boardSize.concat()
       changedBoard[i] = turn
       setBoardSize(changedBoard)
-      changeTurn()
-      console.log('findwinner: ', findWinner(boardSize))
+      setTurn('O')
+      // console.log('findwinner: ', findWinner(boardSize))
+      setWinner(findWinner(changedBoard))  
+      doComputerMove(changedBoard, 'O', 'O')   
+    }    
+  }
+  const computerClick = (i, newboard) => {
+    console.log('computerklik',i, newboard)
+    if (boardSize[i] === '' && !winner) {
+      // console.log('handleClick', i)
+      let changedBoard = newboard.concat()
+      changedBoard[i] = 'O'
+      setBoardSize(changedBoard)
+      setTurn('X')
+      // console.log('findwinner: ', findWinner(boardSize))
       setWinner(findWinner(changedBoard))
     }
-    else return
+    // else alert('computerklik error')
+  }
+  const playAgain = () => {
+    setBoardSize(Array(9).fill(''))
+    setTurn('X')
+    setWinner(null)
   }
   const doComputerMove = (board, sTurn, original) => {
-    console.log('COMPUTER MOVE')
-    let myMove = -1, myScore = -99
+    console.log('COMPUTER MOVE turnit: ', sTurn, original, board)
+    let myMove
+    let myScore = -Infinity
+    let newboard = board.concat()
     for (let i = 0; i < 9; i++) {
       if (board[i] === '') {
-        let newboard = board.concat()
         newboard[i] = sTurn
-        const newTurn = computerTurn(newboard, sTurn, original)
-        if (newTurn > myScore) {
-          myScore = newTurn
+        const newScore = computerTurn(newboard, 'X', original, 0)
+        console.log('newboard', newboard, newScore)
+        // console.log('newscore:', newScore, 'i:', i , 'aiempi myscore ja mymove', myScore, myMove)
+        if (newScore > myScore) {
+          myScore = newScore
           myMove = i
         }
+        newboard[i] = ''
       }
     }
     console.log('comp move: ', myMove)
-    handleClick(myMove)
+    computerClick(myMove, newboard)
   }
   const Square = ({ s, i }) => {
     return (
@@ -50,17 +73,26 @@ const App = () => {
       </div>
     )
   }
-
+  const computerStart = () => {
+    setTurn('O')
+    computerClick(Math.floor(Math.random() * 9), boardSize)
+    // doComputerMove(boardSize, 'O', 'O')    
+  }
+  const Winner = () => {
+    if (!winner) return <button onClick={() => computerStart()}>Computer start</button>
+    else if (winner === 'D') return <> <p>Tasapeli!</p><button onClick={() => playAgain()}>Play again!</button></>
+  else return <> <p>{winner} voitti!</p><button onClick={() => playAgain()}>Play again!</button></>
+  }
   return (
     <>
       <h1>TICTACTOE</h1>
-      {winner ? <p>{winner} voitti!</p> : null}
+  <Winner />
       <div className="board">
         {boardSize.map((s, i) => <Square key={i} s={s} i={i} />)}
       </div>
       <div>
-        <button onClick={() => doComputerMove(boardSize, turn, turn)}>comp</button>
-        <button onClick={() => console.log(boardSize)}>boardi</button>
+        {/* <button onClick={() => doComputerMove(boardSize, turn, turn)}>comp</button>
+        <button onClick={() => console.log(boardSize)}>board</button> */}
       </div>
     </>
   );
--- src/computerMove.js
+++ src/computerMove.js
@@ -3,9 +3,9 @@ const getNextTurn = (t) => {
     if (t === 'X') return 'O'
     else return 'X'
 }
-export const findWinner = (boxes) => {
-    // console.log('find winner')
-    if (!boxes.includes('')) return 'D'
+export const findWinner = (squares) => {
+  
+    // console.log(squares)
     const rows = [
         [0, 1, 2],
         [3, 4, 5],
@@ -18,63 +18,57 @@ export const findWinner = (boxes) => {
     ]
     for (let i = 0; i < rows.length; i++) {
         const [a, b, c] = rows[i]
-        if (boxes[a] !== '' && boxes[a] === boxes[b] && boxes[a] === boxes[c]) {
-            return boxes[a]
+        if (squares[a] !== '' && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
         }
     }
+    if (!squares.includes('')) {
+        return 'D'
+            } 
     return null
 }
 
-const playerTurn = (board, turn, original) => {
-
-    console.log('playerturn: ', board, turn, original)
+export const computerTurn = (board, turn, original, totaalipyoritykset) => {
+    // if(totaalipyoritykset > 55) {
+    //     alert('totaalit ylitty')
+    //     return
+    // } 
     const simuWinner = findWinner(board)
     if (simuWinner) {
-        if (simuWinner === original) return -10
-        else if (simuWinner === 'D') return 0
-        else return 10
-    }
-    else {
-        console.log('playerturn else')
-        let tempBoard = board.concat()
-        let r = 11
-        for (let i = 0; i < 9; i++) {
-            if (tempBoard[i] === '') {
-                let newboard = tempBoard.concat()
-                newboard[i] = turn
-                const newTurn = computerTurn(newboard, getNextTurn(turn), original)
-                if (newTurn < r) r = newTurn
-            }
-        }
-        if(r > 10) console.log('ERROR PLAYERTURN, R YLI 10', r)
-        return r
-    }
-}
-export const computerTurn = (board, turn, original) => {
-    
-    console.log('computerturn: ', board, turn, original)
-    const simuWinner = findWinner(board)
-    if (simuWinner) {
-        if (simuWinner === original) return 10
+        // console.log('winner löyty: ', simuWinner)
+        if (simuWinner === 'O') return 10
         else if (simuWinner === 'D') return 0
         else return -10
     }
     else {
-        console.log('computerturn else')
-        let tempBoard = board.concat()
-        let r = -11
-        for (let i = 0; i < 9; i++) {
-            if (tempBoard[i] === '') {
-                let newboard = tempBoard.concat()
-                newboard[i] = turn
-                const newTurn = playerTurn(newboard, getNextTurn(turn), original)
-                if (newTurn > r) {
-                    r = newTurn
+        if(turn === original) {
+            // if(totaalipyoritykset < 1) console.log('COMPUTERTURN ORIGINAL')
+            let r = -11
+            for (let i = 0; i < 9; i++) {
+                if (board[i] === '') {
+                    board[i] = turn
+                    let newTurn = computerTurn(board, getNextTurn(turn), original, totaalipyoritykset+1)
+                    board[i] = ''
+                    r = Math.max(r, newTurn)
                 }
             }
+            // if(r < -10) alert('ERROR COMPUTERTURN FUNCTIOSSA, R = -11: ', r)
+            return r
         }
-        if(r < -10) console.log('ERROR COMPUTERTURN FUNCTIOSSA, R = -11: ', r)
-        return r
+        else {
+            // if(totaalipyoritykset < 1) console.log('computerturn else')
+            let r = 11
+            for (let i = 0; i < 9; i++) {
+                if (board[i] === '') {
+                    board[i] = turn
+                    let newTurn = computerTurn(board, getNextTurn(turn), original, totaalipyoritykset+1)
+                    board[i] = ''
+                    r = Math.min(r, newTurn)
+                }
+            }
+            // if(r < -10) alert('ERROR COMPUTERTURN FUNCTIOSSA, R = -11: ', r)
+            return r
+        }       
     }
 }
 
